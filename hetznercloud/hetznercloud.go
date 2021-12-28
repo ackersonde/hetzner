24ab89bdc316f2e3ebf37f84f164b4e2df838336
Dan Ackerson <dan@ackerson.de>
2021-12-28 11:44:29 +0100

    cleanup delete msgs

--- hetznercloud/hetznercloud.go
+++ hetznercloud/hetznercloud.go
@@ -16,18 +16,17 @@ func ListAllServers() []*hcloud.Server {
 }
 
 func DeleteServer(serverID int) string {
-	result := fmt.Sprintf("Successfully deleted server [%d] ", serverID)
+	result := fmt.Sprintf("Successfully deleted server %d", serverID)
 
 	client := hcloud.NewClient(hcloud.WithToken(os.Getenv("CTX_HETZNER_API_TOKEN")))
 	server, _, err := client.Server.GetByID(context.Background(), serverID)
 	if err != nil {
-		result = fmt.Sprintf("Server %d doesn't exist!\n", serverID)
-		return result
+		return fmt.Sprintf("Server %d doesn't exist!\n", serverID)
 	}
 
 	_, err = client.Server.Delete(context.Background(), server)
 	if err != nil {
-		result = fmt.Sprintf("Unable to delete server [%d] %s: %s\n", serverID, server.Name, err.Error())
+		return fmt.Sprintf("Unable to delete server [%d] %s: %s\n", serverID, server.Name, err.Error())
 	}
 
 	return result + server.Name
